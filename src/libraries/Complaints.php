4d07ac6de7cd3eff754f06bf6069071266fbb968
Carl Bennett <carl@carlbennett.me>
2017-09-23 18:21:00 +0000

    De-duplicate log line in Complaints::get()

--- src/libraries/Complaints.php
+++ src/libraries/Complaints.php
@@ -12,20 +12,19 @@ class Complaints {
         try {
             $complaintList = Bot::$ts3->complaintList();
             Term::stdout('Getting list of complaints...' . PHP_EOL);
-            Term::stdout(sprintf(
-              'Complaints received: %d' . PHP_EOL, count($complaintList)
-            ));
         } catch (TS3Exception $err) {
             if ($err->getMessage() === 'database empty result set') {
                 $complaintList = array();
-                Term::stdout(sprintf(
-                  'Complaints received: %d' . PHP_EOL, count($complaintList)
-                ));
             } else {
                 $complaintList = null;
                 Term::stderr('Failed to receive complaints list' . PHP_EOL);
                 Term::stderr((string) $err . PHP_EOL);
             }
+        }
+        if ($complaintList !== null) {
+            Term::stdout(sprintf(
+                'Complaints received: %d' . PHP_EOL, count($complaintList)
+            ));
         }
         return $complaintList;
     }
